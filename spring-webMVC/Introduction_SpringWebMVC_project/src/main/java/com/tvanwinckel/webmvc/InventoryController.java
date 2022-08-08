388ad95856a3b9ea18f4e02281191689f0a97690
tvanwinckel <vanwinckelthomas@gmail.com>
2022-08-08 09:59:39 +0200

    Added exercise 3.

--- spring-webMVC/Introduction_SpringWebMVC_project/src/main/java/com/tvanwinckel/webmvc/InventoryController.java
+++ spring-webMVC/Introduction_SpringWebMVC_project/src/main/java/com/tvanwinckel/webmvc/InventoryController.java
@@ -5,13 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(path = "/inventory")
 public class InventoryController {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(InventoryController.class);
 
-    @GetMapping("/inventory")
+    @GetMapping
     public String getInventory(final Model model) {
         LOGGER.info("Received a GET request on the inventoryMethod.");
         model.addAttribute("items", "Empty");
